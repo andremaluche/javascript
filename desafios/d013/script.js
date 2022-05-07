2331fa6f2b9d129c0e6ab848c6663bf580d24ad6
André Maluche <andremaluche@hotmail.com>
2022-05-07 18:32:33 -0300

    Criação do desafio d013

---
+++ desafios/d013/script.js
@@ -0,0 +1,23 @@
+function inicio() {
+    let nome = window.prompt("Qual é o nome do aluno?")
+
+    let nota1 = Number(window.prompt(`Primeira nota de ${nome}:`))
+
+    let nota2 = Number(window.prompt(`Segunda nota de ${nome}:`))
+
+    let res = window.document.querySelector('div#res')
+
+    let media = Number((nota1 + nota2) / 2)
+
+    res.innerHTML = `<h2>Analisando a situação de ${nome}</h2>`
+
+    res.innerHTML += `<p>Com as notas ${nota1} e ${nota2} a <strong>média é ${media}</strong>.</p>`
+
+    if (media < 3) {
+        res.innerHTML += `<p>Com média abaixo de 3,0, o aluno está <span class="reprovado">REPROVADO</span>.</p>`
+    } else if (media >=3 && media <=6) {
+        res.innerHTML += `<p>Com média entre 3,0 e 6,0, o aluno está em <span class="recuperacao">RECUPERAÇÃO</span>.</p>`
+    } else {
+        res.innerHTML += `<p>Com média acima de 6,0 o Aluno está <span class="aprovado">APROVADO</span>.</p>`
+    }
+}
