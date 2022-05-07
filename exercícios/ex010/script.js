ef8f7253236a915ee2b67eaeb8aa9f6a5034dca1
André Maluche <andremaluche@hotmail.com>
2022-05-07 19:02:20 -0300

    Ciração do exercício ex010

---
+++ exercícios/ex010/script.js
@@ -0,0 +1,17 @@
+let res = window.document.querySelector('div#res')
+
+function b1() {
+    res.innerHTML += `<p>Clicou no primeiro botão</p>`
+}
+
+function b2() {
+    res.innerHTML +=`<p>Clicou no segundo botão</p>`
+}
+
+function b3() {
+    res.innerHTML += `<p>Clicou no terceiro botão</p>`
+}
+
+function b4() {
+    res.innerHTML += `<p>Clicou no quarto botão</p>`
+}
