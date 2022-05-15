c06a69a07006026ee4668e73d424b579790ef88a
André Maluche <andremaluche@hotmail.com>
2022-05-15 12:06:48 -0300

    Criação do exercício ex018

---
+++ exercícios/ex018/script.js
@@ -0,0 +1,22 @@
+let res = window.document.querySelector('div#res')
+
+function sortear() {
+    let min = 1
+    let max = 100
+    let dif = max - min
+    let aleatorio = Math.random()
+    computador = min + Math.trunc(dif * aleatorio)
+}
+
+function jogar () {
+    jogador = Number(window.prompt('Qual é o seu palpite?'))
+
+    if (jogador < computador) {
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>MAIOR</strong>!</p>`
+    } else if (jogador > computador) {
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>MENOR</strong>!</p>`
+    } else if (jogador == computador) {
+        res.innerHTML = `<p><strong>PARABÉNS!</strong> Você acertou! Eu tinha sorteado o valor ${computador}!</p>`
+        document.querySelector('button#botao').style.visibility = 'hidden'
+    }
+}
