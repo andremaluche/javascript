e346cc941a4a4644a1960c2b624c2e43be657462
André Maluche <andremaluche@hotmail.com>
2022-05-15 16:34:14 -0300

    Criação do exercício ex019

---
+++ exercícios/ex019/script.js
@@ -0,0 +1,27 @@
+let res = window.document.querySelector('div#res')
+
+function calcular() {
+    let n1 = Number(window.prompt('Primeiro valor:'))
+    let n2 = Number(window.prompt('Segundo valor:'))
+    let opcao = Number(window.prompt(`Valores informados: ${n1} e ${n2}.\nO que vamos fazer?\n[1] Somar\n[2] Subtrair\n[3] Multiplicar\n[4] Dividir`))
+    
+    res.innerHTML = `<h2>Calculando...</h2>`
+
+    switch (opcao) {
+        case 1:
+            res.innerHTML = `<p>${n1} + ${n2} = <strong>${n1+n2}</strong></p>`
+            break
+        case 2: 
+            res.innerHTML = `<p>${n1} - ${n2} = <strong>${n1-n2}</strong></p>`
+            break
+        case 3:
+            res.innerHTML = `<p>${n1} * ${n2} = <strong>${n1*n2}</strong></p>`
+            break
+        case 4:
+            res.innerHTML = `<p>${n1} / ${n2} = <strong>${n1/n2}</strong></p>`
+            break
+        default:
+            res.innerHTML = `<p>OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles.</p>`
+            break
+    }
+}
