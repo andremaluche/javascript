241da8196da6970938b94c09c9ee5e97a2e48394
André Maluche <andremaluche@hotmail.com>
2022-05-15 10:30:11 -0300

    Criação do exercício ex015

---
+++ exercícios/ex015/script.js
@@ -0,0 +1,22 @@
+let res = window.document.querySelector('div#res')
+
+function datahora() {
+    let meses = new Array('Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez')
+    let semana = new Array('Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab')
+    let agora = new Date
+    let dia = agora.getDate()
+    let mes = agora.getMonth()
+    let ano = agora.getFullYear()
+    let sem = agora.getDay()
+    let hora = agora.getHours()
+    let min = agora.getMinutes()
+    let seg = agora.getSeconds()
+
+    res.innerHTML = `<p>Dia: <mark>${dia}</mark></p>`
+    res.innerHTML += `<p>Mês: <mark>${meses[mes]}</mark></p>`
+    res.innerHTML += `<p>Ano: <mark>${ano}</mark></p>`
+    res.innerHTML += `<p>Semana: <mark>${semana[sem]}</mark></p>`
+    res.innerHTML += `<p>Hora: <mark>${hora}</mark></p>`
+    res.innerHTML += `<p>Minutos: <mark>${min}</mark></p>`
+    res.innerHTML += `<p>Segundos: <mark>${seg}</mark></p>`
+}
